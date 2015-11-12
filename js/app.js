988d38da25d9a5fad0264f697899c730e5f16fed
sebastianff <sebastianffarago@gmail.com>
2015-11-12 19:22:35 +0100

    Shaping up

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 (function(){
-var app = angular.module('store',[]);//Here we are declaring a module. A module contains different parts of our app.
+
+	var app = angular.module('store',[]);//Here we are declaring a module. A module contains different parts of our app.
+
+	app.controller('StoreController',function(){//This is a controller, Here we define our apps behaviour.
+		this.products = gems;
+	});
+
+	var gems = [
+	{
+		name:'Dodecahedron',
+		price:2.95,
+		description:'A fine piece of art',
+		canPurchase:true,
+		soldOut:false
+	}
+	]
+
 })();
